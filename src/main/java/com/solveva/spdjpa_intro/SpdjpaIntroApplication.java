d33ab8ab65649dea842b9ac9e0b1270039794ded
nikolai.ponomarev <extern.ponomarev_nikolai@allianz.com>
2025-05-26 17:19:34 +0400

    init commit

---
+++ src/main/java/com/solveva/spdjpa_intro/SpdjpaIntroApplication.java
@@ -0,0 +1,13 @@
+package com.solveva.spdjpa_intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpdjpaIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpdjpaIntroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solveva/spdjpa_intro/SpdjpaIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solveva.spdjpa_intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpdjpaIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
