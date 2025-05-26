0424d4d000f075936a34949130b6761afdb46744
nikolai.ponomarev <extern.ponomarev_nikolai@allianz.com>
2025-05-26 17:33:42 +0400

    add jpa entity

---
+++ src/main/java/com/solveva/spdjpa_intro/domain/Book.java
@@ -0,0 +1,58 @@
+package com.solveva.spdjpa_intro.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String isbn;
+    private String publisher;
+
+    public Book() {}
+
+    public Book(String isbn, String title, String publisher) {
+        this.isbn = isbn;
+        this.title = title;
+        this.publisher = publisher;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+}
